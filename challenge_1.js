c9604457284e64791952a35628f0f5a57c60965d
Dewald Breed <brddew8@protonmail.com>
2023-10-24 11:40:45 +0200

    Completed challenge_1

--- challenge_1.js
+++ challenge_1.js
@@ -3,8 +3,13 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number == secondaryPhone
+const primaryValid = isFinite(parseInt(primaryPhone)) 
+const secondaryValid = isFinite(parseInt(secondaryPhone))
+
+/* Alternative Solution
+ const primaryValid = typeof parseInt(primaryPhone) === 'number' && !isNaN(parseInt(primaryPhone))
+ const secondaryValid = typeof parseInt(secondaryPhone) === 'number' && !isNaN(parseInt(secondaryPhone))
+*/
 
 console.log('Primary phone is valid numerical string:', primaryValid)
-console.log('Secondary phone is valid numerical string:', secondaryValid )
+console.log('Secondary phone is valid numerical string:', secondaryValid)
